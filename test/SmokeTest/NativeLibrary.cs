81a498e5c6aaa2d1695b617acbbf71bb2c930991
José Simões <jose.simoes@eclo.solutions>
2026-06-24 14:15:05 +0100

    Re-host MDP pipeline as single GenerateNanoPeFile target (#4)
    ***NO_CI***

---
+++ test/SmokeTest/NativeLibrary.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+namespace SmokeTest
+{
+    internal class NativeLibrary
+    {
+        public void Method()
+        {
+            NativeMethod();
+
+            byte a = 0;
+            ushort b = 0;
+            NativeMethodWithReferenceParameters(ref a, ref b);
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeMethod();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeMethodWithReferenceParameters(ref byte refByteParam, ref ushort refUshortParam);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void NativeStaticMethod();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern byte NativeStaticMethodReturningByte(char charParam);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern unsafe void MethodWithPointerParm(void* pointer, int length);
+    }
+}
